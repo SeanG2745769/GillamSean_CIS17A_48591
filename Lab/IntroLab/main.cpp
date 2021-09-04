8a176f3facea7eb4de77ef48e47ef0b39158178a
SeanG2745769 <sean.gillam.git@gmail.com>
2021-09-04 12:38:51 -0700

    Signed-off-by: SeanG2745769 <sean.gillam.git@gmail.com>

---
+++ Lab/IntroLab/main.cpp
@@ -0,0 +1,16 @@
+/* 
+ * File:   main.cpp
+ * Author: seang
+ *
+ * Created on September 4, 2021, 12:35 PM
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+int main() {
+    cout << "Hello World!";
+    return 0;
+}
+
